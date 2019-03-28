de97dc8e4a48730b5a187732d7b180306dce78e7
Greg Berger <berger.gregory@gmail.com>
2019-03-28 14:49:21 +0100

    Update config.h

--- code/03.bme680_adafruit.io/bme680_adafruit/config.h
+++ code/03.bme680_adafruit.io/bme680_adafruit/config.h
@@ -1,9 +1,9 @@
 // instructions au compilateur permettant de définir les différents paramètres de connexion
-#define IO_USERNAME "greberg"
-#define IO_KEY "29100448ffe24f73a0b6ee096bd043da"
+#define IO_USERNAME ""
+#define IO_KEY ""
 
-#define WIFI_SSID "3kd.be"
-#define WIFI_PASS "david3000"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 #include "AdafruitIO_WiFi.h"
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
